8b3800228022c691220125ee75da1d1ab2231222
liliane.lewis <liliane.lewis@gmail.com>
2025-02-28 15:40:30 +0200

    Exercise 1

---
+++ Crash_Course/Learn_Python_Fundamentals/Python_Introduction/Exercise_1.py
@@ -0,0 +1,6 @@
+###########################################################
+#Exercise 1: Variable Declaration and input() and print()
+# 1. Create a variable named age and use the input() function to ask the user to enter their age.
+# 2. Print the value of the age variable.
+age = input("What is your age?\n")
+print("Age: ", age)
