e3a4cd41cde7f2d675ce4cf810152a6f00f88806
liliane.lewis <liliane.lewis@gmail.com>
2025-03-04 21:12:58 +0200

    Function Exercises

---
+++ Crash_Course/Learn_Python_Fundamentals/Python_functions/Exercise_1.py
@@ -0,0 +1,8 @@
+# Write a function add_two_numbers that takes two numbers as parameters and returns their sum.
+
+
+def add_two_numbers(num1,num2):
+    return num1 + num2
+
+print(add_two_numbers(3, 5))  # Output: 8
+print(add_two_numbers(10, 20))  # Output: 30
---
+++ Crash_Course/Learn_Python_Fundamentals/Python_functions/Exercise_2.py
@@ -0,0 +1,10 @@
+# Write a function greet that takes one parameter, a person’s name, and prints a greeting message like “Hello, [name]!”.
+
+
+
+def greet(name):
+    return(f"Hello, {name}!")
+
+print(greet("Alice"))  # Output: "Hello, Alice!"
+print(greet("Bob"))    # Output: "Hello, Bob!"
+
