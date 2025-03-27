545329a8cb49f70042dde6d9c22871387ff8060a
liliane.lewis <liliane.lewis@gmail.com>
2025-03-27 12:25:08 +0200

    Week 3 - Day 2 - ExercisesXPNinja

--- Bootcamp/Week3/Day2/ExercisesXPNinja.py
+++ Bootcamp/Week3/Day2/ExercisesXPNinja.py
@@ -16,6 +16,19 @@
 #    Any live cell with more than three live neighbours dies, as if by overpopulation.
 #    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
+#Using these rules, implement the Game. (Hint: use Classes !!!!)
+#Use a few different initial states to see how the game ends.
+#
+#Notes:
+#
+#    Display the grid after each generation
+#    The end of the game is fully determined by the initial state. So have it pass through your program and see how it ends.
+#    Be creative, but use classes
+#    The game can have fixed borders and can also have moving borders. First implement the fixed borders. Each “live” cell that is going out of the border, exits the game.
+#   Bonus: Make the game with ever expandable borders, make the maximum border size a very large number(10,000) so you won’t cause a memory overflow
+
+#TODO: Bonus
+
 
 class Board:
     def __init__(self, board_x_size, board_y_size):
